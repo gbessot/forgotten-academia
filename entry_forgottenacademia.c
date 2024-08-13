a561179e0d3570e971ed8e81b775d513c7b0bfc3
BESSOT Gaëtan <gaetanbessot.officiel@gmail.com>
2024-08-13 15:59:20 +0200

    player, sprites and entities

--- entry_forgottenacademia.c
+++ entry_forgottenacademia.c
@@ -1,36 +1,169 @@
+typedef enum SpriteID {
+	SPRITE_NIL,
+	SPRITE_PLAYER,
+	SPRITE_CIRCLE,
+	SPRITE_SKELETON,
+	SPRITE_MAX
+} SpriteID;
+
+#define SPRITE_SIZE 16.0
+typedef struct Sprite {
+	Gfx_Image* 	image;
+	Vector2 	size;
+} Sprite;
+
+Sprite sprites[SPRITE_MAX];
+Sprite* get_sprite(SpriteID id) {
+	if(id >= 0 && id < SPRITE_MAX) {
+		return &sprites[id];
+	}
+	return &sprites[0];
+}
+
+typedef enum EntityType {
+	ENTITY_PLAYER,
+	ENTITY_CIRCLE,
+	ENTITY_SKELETON
+} EntityType;
+
+typedef struct Entity {
+	bool 		is_valid;
+	EntityType 	type;
+	SpriteID 	sprite_id;
+	Vector2 	pos;
+} Entity;
+
+#define MAX_ENTITY_PER_WORLD 1024
+typedef struct World {
+	Entity entities[MAX_ENTITY_PER_WORLD];
+} World;
+World* world = NULL;
+
+Entity* entity_create() {
+	Entity* found_entity = NULL;
+	for(int i = 0; i < MAX_ENTITY_PER_WORLD; i++) {
+		Entity* existing_entity = &world->entities[i];
+		if(!existing_entity->is_valid) {
+			found_entity = existing_entity;
+			break;
+		}
+	}
+	assert(found_entity, "Max entity per world exceeded.");
+	found_entity->is_valid = true;
+	return found_entity;
+}
+
+void entity_destroy(Entity* entity) {
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_player(Entity* entity) {
+	entity->type 		= ENTITY_PLAYER;
+	entity->sprite_id 	= SPRITE_PLAYER;
+}
+
+void setup_circle(Entity* entity) {
+	entity->type 		= ENTITY_CIRCLE;
+	entity->sprite_id 	= SPRITE_CIRCLE;
+}
+
+void setup_skeleton(Entity* entity) {
+	entity->type 		= ENTITY_SKELETON;
+	entity->sprite_id 	= SPRITE_SKELETON;
+}
 
 int entry(int argc, char **argv) {
 	
-	window.title = STR("Minimal Game Example");
-	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
-	window.scaled_height = 720; 
-	window.x = 200;
-	window.y = 90;
-	window.clear_color = hex_to_rgba(0x6495EDff);
+	window.title 			= STR("Forgotten Academia");
+	window.scaled_width 	= 1280;
+	window.scaled_height 	= 720; 
+	window.x 				= 200;
+	window.y 				= 200;
+	window.clear_color 		= hex_to_rgba(0x4f4263ff);
 
-	float64 last_time = os_get_current_time_in_seconds();
+	float64 last_time 		= os_get_current_time_in_seconds();
+	float64 seconds_count 	= 0.0;
+	int frame_count 		= 0;
+
+	sprites[SPRITE_PLAYER] 		= (Sprite){ .image=load_image_from_disk(STR("player.png"), 		get_heap_allocator()), .size=v2(SPRITE_SIZE, SPRITE_SIZE) };
+	sprites[SPRITE_CIRCLE] 		= (Sprite){ .image=load_image_from_disk(STR("circle.png"), 		get_heap_allocator()), .size=v2(SPRITE_SIZE, SPRITE_SIZE) };
+	sprites[SPRITE_SKELETON]	= (Sprite){ .image=load_image_from_disk(STR("skeleton.png"), 	get_heap_allocator()), .size=v2(SPRITE_SIZE, SPRITE_SIZE) };
+
+	world = alloc(get_heap_allocator(), sizeof(World));
+	memset(world, 0, sizeof(World));
+
+	Entity* player_entity = entity_create();
+	setup_player(player_entity);
+
+	for(int i = 0; i < 10; i++) {
+		Entity* entity = entity_create();
+		setup_circle(entity);
+		entity->pos = v2(get_random_float32_in_range(-300, 300), get_random_float32_in_range(-200, 200));
+	}
+
+	for(int i = 0; i < 10; i++) {
+		Entity* entity = entity_create();
+		setup_skeleton(entity);
+		entity->pos = v2(get_random_float32_in_range(-300, 300), get_random_float32_in_range(-200, 200));
+	}
+
 	while (!window.should_close) {
-		float64 now = os_get_current_time_in_seconds();
-		if ((int)now != (int)last_time) log("%.2f FPS\n%.2fms", 1.0/(now-last_time), (now-last_time)*1000);
-		last_time = now;
-		
 		reset_temporary_storage();
-		
-		Matrix4 rect_xform = m4_scalar(1.0);
-		rect_xform         = m4_rotate_z(rect_xform, (f32)now);
-		rect_xform         = m4_translate(rect_xform, v3(-.25f, -.25f, 0));
-		draw_rect_xform(rect_xform, v2(.5f, .5f), COLOR_GREEN);
-		
-		draw_rect(v2(sin(now), -.8), v2(.5, .25), COLOR_RED);
-		
-		float aspect = (f32)window.width/(f32)window.height;
-		float mx = (input_frame.mouse_x/(f32)window.width  * 2.0 - 1.0)*aspect;
-		float my = input_frame.mouse_y/(f32)window.height * 2.0 - 1.0;
-		
-		draw_line(v2(-.75, -.75), v2(mx, my), 0.005, COLOR_WHITE);
-		
 		os_update(); 
 		gfx_update();
+
+		// :view
+		float zoom 				= 3;
+		draw_frame.projection 	= m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
+		draw_frame.view 		= m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
+
+		// :time
+		float64 now 	= os_get_current_time_in_seconds();
+		float64 delta_t	= now - last_time;
+		last_time 		= now;
+
+		// :rendering
+		for(int i = 0; i < MAX_ENTITY_PER_WORLD; i++) {
+			Entity* entity = &world->entities[i];
+			if(entity->is_valid) {
+				Sprite* sprite		= get_sprite(entity->sprite_id);
+				Matrix4 xform		= m4_scalar(1.0);
+				xform				= m4_translate(xform, v3(entity->pos.x, entity->pos.y, 0));
+				xform				= m4_translate(xform, v3(sprite->size.x * -0.5, 0.0, 0));
+				draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+			}
+		}
+
+		// :input
+		if(is_key_just_pressed(KEY_ESCAPE)) {
+			window.should_close = true;
+		}
+
+		Vector2 input_axis = v2(0, 0);
+		if (is_key_down('Q')) {
+			input_axis.x -= 1.0;
+		}
+		if (is_key_down('D')) {
+			input_axis.x += 1.0;
+		}
+		if (is_key_down('S')) {
+			input_axis.y -= 1.0;
+		}
+		if (is_key_down('Z')) {
+			input_axis.y += 1.0;
+		}
+		input_axis = v2_normalize(input_axis);
+
+		player_entity->pos = v2_add(player_entity->pos, v2_mulf(input_axis, 100 * delta_t));
+
+		// :fps
+		seconds_count 	+= delta_t;
+		frame_count 	+= 1;
+		if (seconds_count > 1.0) {
+			log("fps: %i", frame_count);
+			seconds_count 	= 0.0;
+			frame_count 	= 0;
+		}
 	}
 
 	return 0;
